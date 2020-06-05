36c2662a0c1c175d14003fb71979509ddcba6a77
vecin2 <vecin2@gmail.com>
2020-06-04 23:45:32 -0400

    set persistent_history

--- emtask/__main__.py
+++ emtask/__main__.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
-from nubia import Nubia, Options
-import  emtask.sql.nubia_commands
-import  emtask.ced.nubia_commands
+
 import colorama
+from nubia import Nubia, Options
 
+import emtask.ced.nubia_commands
+import emtask.sql.nubia_commands
 
-colorama.init() #allow termcolor to show colors on windows
+colorama.init()  # allow termcolor to show colors on windows
+
 
 def main():
     shell = Nubia(
         name="nubia_example",
-        command_pkgs=[emtask.sql.nubia_commands,
-                      emtask.ced.nubia_commands
-                      ],
-        options=Options(persistent_history=False)
+        command_pkgs=[emtask.sql.nubia_commands, emtask.ced.nubia_commands],
+        options=Options(persistent_history=True),
     )
     sys.exit(shell.run())
 
